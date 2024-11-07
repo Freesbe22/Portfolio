5be3aaf24ab148e12afb8e3b6a2dcda9aac53b38
Freesbe22 <61194179+Freesbe22@users.noreply.github.com>
2024-11-07 11:32:20 +0100

    Ajout : Section A Propos, image de fond, lorem ipsum

--- resources/js/Components/Welcome/Sections/Apropos.vue
+++ resources/js/Components/Welcome/Sections/Apropos.vue
@@ -10,13 +10,19 @@
             </div>
             <div class="row text-light mt-4">
                 <p>
-                    lorem ipsum
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus metus nisl, volutpat sed neque at,
+                    efficitur aliquam lorem. Sed sagittis tincidunt turpis, non sagittis turpis placerat vulputate.
+                    Nulla ullamcorper nunc in elit gravida, suscipit placerat lacus semper. Fusce vitae turpis commodo,
+                    ultricies est ut, fermentum urna. Ut sit amet rutrum odio, in egestas lacus. Nulla id nulla diam.
+                    Morbi libero erat, euismod et sapien a, finibus hendrerit nisl. Morbi quis ante vel felis iaculis
+                    scelerisque. Duis euismod sed nulla id cursus. Nulla facilisi. Cras tempus neque ut lorem varius
+                    blandit. Donec fermentum mauris eget eros imperdiet, et faucibus dui dictum. Praesent facilisis
+                    finibus turpis vel lacinia.
+                    (Ce texte sera renseigné ultérieurement)
                 </p>
             </div>
-            <div class="row mt-4">
-                <div>
-                    <img src="/images/Apropos.jpg" />
-                </div>
+            <div class="row mt-4" id="img-row">
+                <img src="../../../../images/Apropos_2.jpg" />
             </div>
         </div>
     </section>
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,17 +5,6 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
-// /* import the fontawesome core */
-// import { library } from '@fortawesome/fontawesome-svg-core'
-
-// /* import font awesome icon component */
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-// /* import specific icons */
-// import { faHome,faBars,faCaretLeft,faCaretRight,faCheckCircle } from '@fortawesome/free-solid-svg-icons'
-
-// /* add icons to the library */
-// library.add(faHome,faBars,faCaretLeft,faCaretRight,faCheckCircle);
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 import.meta.glob([
